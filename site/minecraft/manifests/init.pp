c38a7a92ab3824c76f9271adb3d6b352e888c7bf
diegolopezronda <62171420+diegolopezronda@users.noreply.github.com>
2021-08-12 11:40:45 +1200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,6 @@
 class minecraft(
   $url = 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
-  $install_dir = '/opt/minecraft/'
+  $install_dir = '/opt/minecraft'
 ){
   file {'/opt/minecraft':
     ensure => directory,
@@ -13,7 +13,7 @@ class minecraft(
   package {'java':
     ensure => present,
   }
-  file {"${install_dir}eula.txt":
+  file {"${install_dir}/eula.txt":
     ensure =>file,
     content => 'eula=true',
   }
@@ -26,6 +26,6 @@ class minecraft(
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File["${install_dir}eula.txt"],File['/etc/systemd/system/minecraft.service']]
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']]
   }
 }

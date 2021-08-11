2112154dc8a21479846f1cd76e785a075e01de85
diegolopezronda <62171420+diegolopezronda@users.noreply.github.com>
2021-08-12 01:07:55 +1200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7c9sdNhKngyCp4X4y8G59oE3xZVSucRYydOw6yZGspcYcVKu7aFtXcyAviFPx6Z2l0r+S+87a64gqIAQodRgRTKD7XnC1+ztaBHEi9ibWb/YNt2W9DzJyFk9vojknJy93oyELcmYZesLxOZgykjvpNzeiJFVe9g7NPWx4PpJR6IhnimwTMqxuPISvJzRbcVV2ciuZrFhrBfywj/Pg4BJk2Nsw21sRwmnVRepX7CKok78z1k2hGydcoTsHNT+xI+6Hc783ebu6JUWlu48wBO6VDUzsiJe7zflHdjgisr+jO1jrD482ZVy6TeUPUkObBgGz+j3KIK3d6NpjDeqAcBub root@master.puppet.vm",
+		key    => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7c9sdNhKngyCp4X4y8G59oE3xZVSucRYydOw6yZGspcYcVKu7aFtXcyAviFPx6Z2l0r+S+87a64gqIAQodRgRTKD7XnC1+ztaBHEi9ibWb/YNt2W9DzJyFk9vojknJy93oyELcmYZesLxOZgykjvpNzeiJFVe9g7NPWx4PpJR6IhnimwTMqxuPISvJzRbcVV2ciuZrFhrBfywj/Pg4BJk2Nsw21sRwmnVRepX7CKok78z1k2hGydcoTsHNT+xI+6Hc783ebu6JUWlu48wBO6VDUzsiJe7zflHdjgisr+jO1jrD482ZVy6TeUPUkObBgGz+j3KIK3d6NpjDeqAcBub root@master.puppet.vm"
 	}  
 }

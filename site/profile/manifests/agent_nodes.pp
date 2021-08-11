2eceb77028b301c832aa2b3c161c722862ac236d
diegolopezronda <62171420+diegolopezronda@users.noreply.github.com>
2021-08-11 21:34:48 +1200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,9 +1,5 @@
-class profile::agent_nodes{
-  include 'dockeragent'
-  dockeragent::node {
-    'web.puppet.vm':
-  }
-  dockeragent::node {
-    'db.puppet.vm':
-  }
+class profile::agent_nodes {
+  include dockeragent
+  dockeragent::node { 'web.puppet.vm': }
+  dockeragent::node { 'db.puppet.vm': }
 }
